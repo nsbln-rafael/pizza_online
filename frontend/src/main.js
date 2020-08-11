803fb770d6dd8bfb89a60256a7a1227c6ad65f0c
Nasibullin Rafael <nsbln.rafael@gmail.com>
2020-08-11 18:53:50 +0600

    Add bootstrap, bootstrap-vue to vuejs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,16 @@ import VueRouter from "vue-router";
 import Vuex from "vuex";
 import {routes} from "./routes";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const router = new VueRouter({
     mode: 'history',
